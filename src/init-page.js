0e96501e84e1157cf4db8dbd3b3f29fd0766bf90
JodieFlacko <96775468+JodieFlacko@users.noreply.github.com>
2025-09-24 12:36:58 +0200

    correctly load initial page

---
+++ src/init-page.js
@@ -0,0 +1,44 @@
+function initialPageLoad(content){
+  const pageContainer = document.createElement("div");
+  pageContainer.className = "init-page-container"; 
+  
+  const slider = createSlider()
+  pageContainer.appendChild(slider);
+  content.appendChild(pageContainer);
+}
+
+function createSlider(){
+  const imageURLs = [
+    "https://images.pexels.com/photos/16994473/pexels-photo-16994473.jpeg",
+    "https://images.pexels.com/photos/30305404/pexels-photo-30305404.jpeg",
+    "https://images.pexels.com/photos/33240983/pexels-photo-33240983.jpeg",
+  ];
+
+  const sliderWrapper = document.createElement("div");
+  sliderWrapper.className = "slider-wrapper";
+
+  const slider = document.createElement("div");
+  slider.className = "slider";
+
+  const sliderNav = document.createElement("div");
+  sliderNav.className = "slider-nav";
+
+  for(let i = 0; i < 3; i++){
+    const sliderImage = document.createElement("img");
+    sliderImage.id = "slide " + i;
+    sliderImage.src = imageURLs[i];
+    sliderImage.alt = "3D rendering of pizza image";
+    slider.appendChild(sliderImage);
+
+    const navBullet = document.createElement("a");
+    navBullet.href = "#slide " + i;
+    sliderNav.appendChild(navBullet);
+  };
+
+  sliderWrapper.appendChild(slider);
+  sliderWrapper.appendChild(sliderNav);
+
+  return sliderWrapper;
+};
+
+export { initialPageLoad };
